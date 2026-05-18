d73fbb39170055464d3987f416637d138c92aae8
Tristan Samuk <tsamuk@gmail.com>
2026-05-18 09:04:33 -0400

    Install husky precommit hook and lint-staged

--- eslint.config.js
+++ eslint.config.js
@@ -3,9 +3,11 @@
 import js from "@eslint/js";
 import { defineConfig } from "eslint/config";
 import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 export default defineConfig(
   { ignores: ["dist/**", "node_modules/**"] },
   js.configs.recommended,
   tseslint.configs.strict,
+  eslintConfigPrettier
 );
